4581d4ecad6ead9daffa70f01df0b86bdbf7ddc4
tschomacker <thorben.schomacker@gmail.com>
2025-02-25 17:18:15 +0100

    #9 + portability

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Various scripts used in the Formulae - Litterae - Chartae Project
 
 ## Recommendations:
 - Create a Python virtualenv (e.g., `virtualenv --python=python3 .venv`) and use it `source .venv/bin/activate`
+
+### Run a script:    
+1. activate the virtualenv (e.g., `source .venv/bin/activate`) 
+2. install the requirements via `pip install -r requirements.txt` within in the venv
+3. If the requirements have been installed properly, you can launch `python3 <your_script>.py` 
--- corpus_transformation_scripts/Formulae/mss_edition_csv_to_xml.py
+++ corpus_transformation_scripts/Formulae/mss_edition_csv_to_xml.py
@@ -5,47 +5,32 @@ from lxml.builder import E
 from lxml import etree
 import os
 import logging
-logging.basicConfig(encoding='utf-8', level=logging.INFO)
+from util import get_logger
+import errno
 
-input_path = argv[1]
-formulae_collections_md_file = argv[2]
-manuscript_collections_md_file = argv[3]
-output_folder_hss_editionen = '/home/thorben.schomacker/git/scripts/formel_transform/output/auvergne/hss_editionen.xml' #argv[3]
+def xslsx_to_csv(input_path,logging=None) -> str:
+    """
+    Convert a xslsx-file located at input_path and convert it to csv-file at input_path.
+    """
+    input_file = find_csv(input_path, file_ending='*.xlsx')
+    import pandas as pd 
+    df = pd.DataFrame(pd.read_excel(input_file))
+    output_file = input_file.replace('xlsx', 'csv') 
+    df.to_csv(output_file)
+    if logging is not None:
+        logging.info('converted {input} to {output}'.format(input=input_file,output=output_file))
+    return output_file
 
-input_encoding = 'utf-8'
 
-def find_csv(input_path):
+def find_csv(input_path, file_ending='*.csv'):
     import fnmatch
     csv_list = []
     for file in os.listdir(input_path):
-        if fnmatch.fnmatch(file, '*.csv'):
+        if fnmatch.fnmatch(file, file_ending):
             csv_list.append(os.path.join(input_path, file))
     if 1==len(csv_list): return csv_list[0]
-    if 0==len(csv_list): raise Exception("No csv-file found")
+    if 0==len(csv_list): raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), input_path+'/'+file_ending)
     if 1<len(csv_list): raise Exception("There is more than one csv-file")
-
-if not 'csv' in input_path:
-    csv_file = find_csv(input_path)
-    
-else:
-    csv_file = input_path
-
-logging.info('csv-file:'+csv_file)
-
-ns = {'tei': 'http://www.tei-c.org/ns/1.0', 'cpt': 'http://purl.org/capitains/ns/1.0#', 'dc': 'http://purl.org/dc/elements/1.1/', 'dct': 'http://purl.org/dc/terms/', 'bib': 'http://bibliotek-o.org/1.0/ontology/'}
-
-ed_bib_info = {'Zeu': ('Zeu', 'Zeumer, Karl: Formulae Merowingici et Karolini aevi, Hannover 1882.'),
-               'Zeua': ('Zeu&lt;span class="verso-recto"&gt;a&lt;/span&gt;', 'Zeumer, Karl: Über die älteren fränkischen Formelsammlungen, in: Neues Archiv der Gesellschaft für ältere deutsche Geschichtskunde 6 (1881), S. 9–115.'),
-               'Udd': ('Udd', 'Uddholm, Alf: Marculfi formularium libri duo, 1962 (Collectio scriptorum veterum Upsaliensis).'),
-               'Lin': ('Lin', 'Lindenbrog, Friedrich: Codex legum antiquarum. In quo continentur Leges Wisigothorum, Edictum Theodorici, Lex Burgundionum, Lex Allamannorum, Lex Baiuvariorum, Decretum Tassilonis, Lex Ripuariorum, Lex Saxonum, Lex Angliorum, Lex Frisionum, Lex Langobardorum, Constitutiones siculae, Capitulare Caroli, quibus accedunt Formulae solennes priscae publicorum privatorumque negotium, Frankfurt a.M. 1613'),
-               'Roz': ('Roz', "Rozière, Eugène de: Recueil des formules usitées dans l'empire des Francs du Ve au Xe siècle, Paris 1859-1871."),
-               'Dav/Fou': ('Dav/Fou', 'Davies, Wendy und Paul Fouracre: The settlement of disputes in early medieval Europe, Cambridge 1986.'),
-               'Roc': ('Roc', 'Rockinger, Ludwig von (Hg.): Drei Formelsammlungen aus der Zeit der Karolinger. Aus Münchner Handschriften mitgetheilt, München 1858.'),
-               'Mab': ('Mab', 'Mabillon, Jean: Librorum De Re Diplomatica Supplementum : In Quo Archetypa In His Libris pro regulis proposita, ipsaeque regulae denuo confirmantur, novisque speciminibus et argumentis et illustrantur, Paris 1704.'),
-               'Rio': ('Rio', 'Rio, Alice: The formularies of Angers and Marculf: Two Merovingian legal handbooks, Liverpool 2008 (Translated texts for historians 46).'),
-               'Par': ('Par', "Pardessus, Jean-Marie: Notice sur les manuscrits de formules relatives au droit observé dans l'Empire des Francs, suivie de quatorze formules inédites, in: Bibliothèque de l’école des chartes 4 (1843), S. 1-22."),
-               'Bis': ('Bis', 'Bischoff, Bernhard: Epitaphienformeln für Äbtissinnen (Achtes Jahrhundert), in: Ders. (Hg.), Anecdota Novissima. Texte des vierten bis sechszehnten Jahrhunderts, Stuttgart 1984, S. 152')}
-
 
 def build_urn(s): 
     roman_mapping = {'I': '1', 'II': '2'} 
@@ -135,62 +120,106 @@ def build_editions(s, ed_dict):
             formatted_eds.append(ed)
     return '; '.join(formatted_eds)
 
-form_ms_ed_xml = E.xml()
+def get_csv(input_path:str,logging) -> str:
+    if not 'csv' in input_path:
+        try:
+            return find_csv(input_path)
+        except FileNotFoundError:
+            return xslsx_to_csv(input_path,logging)
+    else:
+        return input_path
 
 
-with open(csv_file, encoding=input_encoding) as f:
-    rows = f.readlines()
+def main():
+    logging = get_logger()
+    input_path = argv[1]
+    formulae_collections_md_file = argv[2]
+    manuscript_collections_md_file = argv[3]
+    output_folder_hss_editionen = '/home/thorben.schomacker/git/scripts/formel_transform/output/auvergne/hss_editionen.xml' #argv[3]
 
-# Map titles to URNs
-form_coll_md = etree.parse(formulae_collections_md_file)
-title_id_dict = dict()
-for f_c in form_coll_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
-    form_corp_md_path = os.path.normpath(os.path.join(os.path.dirname(formulae_collections_md_file), f_c.get('path')))
-    form_corp_md = etree.parse(form_corp_md_path)
-    for f_corp in form_corp_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
-        form_md_path = os.path.normpath(os.path.join(os.path.dirname(form_corp_md_path), f_corp.get('path')))
-        form_md = etree.parse(form_md_path)
-        for c in form_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
-            for t in c.xpath('./dc:type/text()', namespaces=ns):
-                if t  == 'cts:edition':
-                    title_id_dict[re.sub(r' \(lat\).*', '', c.xpath('./dc:title/text()', namespaces=ns)[0])] = c.xpath('./cpt:identifier/text()', namespaces=ns)[0]
-                    title_id_dict[c.xpath('./dc:title/text()', namespaces=ns)[0].replace(' (lat)', '')] = c.xpath('./cpt:identifier/text()', namespaces=ns)[0]
+    input_encoding = 'utf-8'
 
-# Map MS sigla to the HTML needed to show them properly
-ms_coll_md = etree.parse(manuscript_collections_md_file)
-sigla_html_dict = {'Fu†': '&lt;span data-toggle="tooltip" data-boundary="window" id="Fu-verloren-note-tooltip" data-container="body" title="Verlorene Handschrift aus Fulda vgl. Bibliothekskatalog Fulda 16. Jhd. (Vatikan BAV Pal. Lat. 1928) Nr. 238"&gt;&lt;a href="https://digi.ub.uni-heidelberg.de/diglit/bav_pal_lat_1928/0099/image,info" target="_blank"&gt;Fu† ↗&lt;/a&gt;&lt;/span&gt;'}
-for f_c in ms_coll_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
-    ms_corp_md_path = os.path.normpath(os.path.join(os.path.dirname(manuscript_collections_md_file), f_c.get('path')))
-    ms_corp_md = etree.parse(ms_corp_md_path)
-    ms_title = ms_corp_md.xpath('/cpt:collection/dc:title/text()', namespaces=ns)[0]
-    ms_siglum = ms_corp_md.xpath('/cpt:collection/cpt:structured-metadata/bib:AbbreviatedTitle/text()', namespaces=ns)[0].replace('class="manuscript-number"', 'class="subscript smaller-text"').replace('class="verso-recto"', 'class="superscript smaller-text"')
-    ms_html = '&lt;span data-toggle="tooltip" data-boundary="window" id="{htmlID}" data-container="body" title="{ms_title}"&gt;{ms_siglum}&lt;/span&gt;'.format(htmlID=re.sub(R'<[^>]+>', '', ms_siglum) + '-note-tooltip', ms_title=ms_title, ms_siglum=ms_siglum.replace('<', '&lt;').replace('>', '&gt;'))
-    sigla_html_dict[re.sub(r'<[^>]+>', '', ms_siglum)] = ms_html
+    csv_file = get_csv(input_path,logging)
 
-for r in rows[1:]:
-    cells = r.strip().split('\t')
-    if len(cells) > 1:
-        info_string = build_sigla(cells[1].strip(), sigla_html_dict) + '**' + build_editions(cells[2], ed_bib_info)
-        if len(cells) > 3:
-            info_string += '**' + '**'.join(cells[3:])
-        cells[0] = re.sub(r'Flavigny Pa 7 (\D)', r'Flavigny Pa 7\1', cells[0])
-        form_ms_ed_xml.append(E.formula(info_string, n=title_id_dict[cells[0].strip()]))
-    # form_ms_ed_dict[build_urn(formula)] = {'manuscripts': build_sigla(mss), 'editions': build_editions(eds)}
+    logging.info('csv-file: '+csv_file)
 
-xml_string = etree.tostring(form_ms_ed_xml, pretty_print=True, encoding='unicode')
-xml_string = xml_string.replace('&amp;', '&')
-# for an unknown reason matt used ampersand encoding
-xml_string = xml_string.replace('&lt;', '<')
-xml_string = xml_string.replace('&gt;', '>')
-xml_path = csv_file.replace('.csv', '.xml')
+    ns = {'tei': 'http://www.tei-c.org/ns/1.0', 'cpt': 'http://purl.org/capitains/ns/1.0#', 'dc': 'http://purl.org/dc/elements/1.1/', 'dct': 'http://purl.org/dc/terms/', 'bib': 'http://bibliotek-o.org/1.0/ontology/'}
 
-with open(xml_path, mode='w',encoding='utf-8') as f:
-    #json.dump(form_ms_ed_dict, f, ensure_ascii=False, indent='\t')
-    
-    
-    f.write(xml_string)
-    logging.info('Done! The file was written to: '+xml_path)
+    ed_bib_info = {'Zeu': ('Zeu', 'Zeumer, Karl: Formulae Merowingici et Karolini aevi, Hannover 1882.'),
+                'Zeua': ('Zeu&lt;span class="verso-recto"&gt;a&lt;/span&gt;', 'Zeumer, Karl: Über die älteren fränkischen Formelsammlungen, in: Neues Archiv der Gesellschaft für ältere deutsche Geschichtskunde 6 (1881), S. 9–115.'),
+                'Udd': ('Udd', 'Uddholm, Alf: Marculfi formularium libri duo, 1962 (Collectio scriptorum veterum Upsaliensis).'),
+                'Lin': ('Lin', 'Lindenbrog, Friedrich: Codex legum antiquarum. In quo continentur Leges Wisigothorum, Edictum Theodorici, Lex Burgundionum, Lex Allamannorum, Lex Baiuvariorum, Decretum Tassilonis, Lex Ripuariorum, Lex Saxonum, Lex Angliorum, Lex Frisionum, Lex Langobardorum, Constitutiones siculae, Capitulare Caroli, quibus accedunt Formulae solennes priscae publicorum privatorumque negotium, Frankfurt a.M. 1613'),
+                'Roz': ('Roz', "Rozière, Eugène de: Recueil des formules usitées dans l'empire des Francs du Ve au Xe siècle, Paris 1859-1871."),
+                'Dav/Fou': ('Dav/Fou', 'Davies, Wendy und Paul Fouracre: The settlement of disputes in early medieval Europe, Cambridge 1986.'),
+                'Roc': ('Roc', 'Rockinger, Ludwig von (Hg.): Drei Formelsammlungen aus der Zeit der Karolinger. Aus Münchner Handschriften mitgetheilt, München 1858.'),
+                'Mab': ('Mab', 'Mabillon, Jean: Librorum De Re Diplomatica Supplementum : In Quo Archetypa In His Libris pro regulis proposita, ipsaeque regulae denuo confirmantur, novisque speciminibus et argumentis et illustrantur, Paris 1704.'),
+                'Rio': ('Rio', 'Rio, Alice: The formularies of Angers and Marculf: Two Merovingian legal handbooks, Liverpool 2008 (Translated texts for historians 46).'),
+                'Par': ('Par', "Pardessus, Jean-Marie: Notice sur les manuscrits de formules relatives au droit observé dans l'Empire des Francs, suivie de quatorze formules inédites, in: Bibliothèque de l’école des chartes 4 (1843), S. 1-22."),
+                'Bis': ('Bis', 'Bischoff, Bernhard: Epitaphienformeln für Äbtissinnen (Achtes Jahrhundert), in: Ders. (Hg.), Anecdota Novissima. Texte des vierten bis sechszehnten Jahrhunderts, Stuttgart 1984, S. 152')}
 
-with open(output_folder_hss_editionen, mode='w+',encoding='utf-8') as f:
-    f.write(xml_string)
-    logging.info('Done! The file was written to: '+output_folder_hss_editionen)
+
+
+
+    form_ms_ed_xml = E.xml()
+
+
+    with open(csv_file, encoding=input_encoding) as f:
+        rows = f.readlines()
+
+    # Map titles to URNs
+    form_coll_md = etree.parse(formulae_collections_md_file)
+    title_id_dict = dict()
+    for f_c in form_coll_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
+        form_corp_md_path = os.path.normpath(os.path.join(os.path.dirname(formulae_collections_md_file), f_c.get('path')))
+        if not os.path.isfile(form_corp_md_path): raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), form_corp_md_path)
+        form_corp_md = etree.parse(form_corp_md_path)
+        for f_corp in form_corp_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
+            form_md_path = os.path.normpath(os.path.join(os.path.dirname(form_corp_md_path), f_corp.get('path')))
+            form_md = etree.parse(form_md_path)
+            for c in form_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
+                for t in c.xpath('./dc:type/text()', namespaces=ns):
+                    if t  == 'cts:edition':
+                        title_id_dict[re.sub(r' \(lat\).*', '', c.xpath('./dc:title/text()', namespaces=ns)[0])] = c.xpath('./cpt:identifier/text()', namespaces=ns)[0]
+                        title_id_dict[c.xpath('./dc:title/text()', namespaces=ns)[0].replace(' (lat)', '')] = c.xpath('./cpt:identifier/text()', namespaces=ns)[0]
+
+    # Map MS sigla to the HTML needed to show them properly
+    ms_coll_md = etree.parse(manuscript_collections_md_file)
+    sigla_html_dict = {'Fu†': '&lt;span data-toggle="tooltip" data-boundary="window" id="Fu-verloren-note-tooltip" data-container="body" title="Verlorene Handschrift aus Fulda vgl. Bibliothekskatalog Fulda 16. Jhd. (Vatikan BAV Pal. Lat. 1928) Nr. 238"&gt;&lt;a href="https://digi.ub.uni-heidelberg.de/diglit/bav_pal_lat_1928/0099/image,info" target="_blank"&gt;Fu† ↗&lt;/a&gt;&lt;/span&gt;'}
+    for f_c in ms_coll_md.xpath('/cpt:collection/cpt:members/cpt:collection', namespaces=ns):
+        ms_corp_md_path = os.path.normpath(os.path.join(os.path.dirname(manuscript_collections_md_file), f_c.get('path')))
+        ms_corp_md = etree.parse(ms_corp_md_path)
+        ms_title = ms_corp_md.xpath('/cpt:collection/dc:title/text()', namespaces=ns)[0]
+        ms_siglum = ms_corp_md.xpath('/cpt:collection/cpt:structured-metadata/bib:AbbreviatedTitle/text()', namespaces=ns)[0].replace('class="manuscript-number"', 'class="subscript smaller-text"').replace('class="verso-recto"', 'class="superscript smaller-text"')
+        ms_html = '&lt;span data-toggle="tooltip" data-boundary="window" id="{htmlID}" data-container="body" title="{ms_title}"&gt;{ms_siglum}&lt;/span&gt;'.format(htmlID=re.sub(R'<[^>]+>', '', ms_siglum) + '-note-tooltip', ms_title=ms_title, ms_siglum=ms_siglum.replace('<', '&lt;').replace('>', '&gt;'))
+        sigla_html_dict[re.sub(r'<[^>]+>', '', ms_siglum)] = ms_html
+
+    for r in rows[1:]:
+        cells = r.strip().split('\t')
+        if len(cells) > 1:
+            info_string = build_sigla(cells[1].strip(), sigla_html_dict) + '**' + build_editions(cells[2], ed_bib_info)
+            if len(cells) > 3:
+                info_string += '**' + '**'.join(cells[3:])
+            cells[0] = re.sub(r'Flavigny Pa 7 (\D)', r'Flavigny Pa 7\1', cells[0])
+            form_ms_ed_xml.append(E.formula(info_string, n=title_id_dict[cells[0].strip()]))
+        # form_ms_ed_dict[build_urn(formula)] = {'manuscripts': build_sigla(mss), 'editions': build_editions(eds)}
+
+    xml_string = etree.tostring(form_ms_ed_xml, pretty_print=True, encoding='unicode')
+    xml_string = xml_string.replace('&amp;', '&')
+    # for an unknown reason matt used ampersand encoding
+    xml_string = xml_string.replace('&lt;', '<')
+    xml_string = xml_string.replace('&gt;', '>')
+    xml_path = csv_file.replace('.csv', '.xml')
+
+    with open(xml_path, mode='w',encoding='utf-8') as f:
+        #json.dump(form_ms_ed_dict, f, ensure_ascii=False, indent='\t')
+        
+        
+        f.write(xml_string)
+        logging.info('Done! The file was written to: '+xml_path)
+
+    with open(output_folder_hss_editionen, mode='w+',encoding='utf-8') as f:
+        f.write(xml_string)
+        logging.info('Done! The file was written to: '+output_folder_hss_editionen)
+
+if __name__ == '__main__':
+    main()
