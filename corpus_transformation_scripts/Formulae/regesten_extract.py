609d181bc31a70124d189f9eef3410f7ec075464
tschomacker <thorben.schomacker@gmail.com>
2025-03-19 19:45:50 +0100

    #12 wip

--- corpus_transformation_scripts/Formulae/regesten_extract.py
+++ corpus_transformation_scripts/Formulae/regesten_extract.py
@@ -12,7 +12,7 @@ def get_corpus_information(input_tei_path)-> dict:
     if len(file_parts) == 3 or len(file_parts) == 2:
         corpus_information_dict["corpus"] = file_parts[1].lower()
         if len(file_parts) == 3:
-            corpus_information_dict["subcorpus_index"] = file_parts[2]
+            corpus_information_dict["subcorpus_index"] = file_parts[2].lower()
         else:
             corpus_information_dict["subcorpus_index"] = ""
         return corpus_information_dict
@@ -24,7 +24,7 @@ def get_corpus_information(input_tei_path)-> dict:
 if __name__ == '__main__':
     parser=argparse.ArgumentParser(description="Script to transform DOCX files with regesten to a correspondig tei file")
     default_saxon_location = make_proper_path("~/Downloads/SaxonHE9-8-0-11J/saxon9he.jar")
-    logging = get_logger()
+    logger = get_logger()
     saxon_location = default_saxon_location
     transformation_file =  make_proper_path("~/git/scripts/corpus_transformation_scripts/Formulae/regesten_extract.xsl")
     input_docx_path = make_proper_path("~/git/scripts/formel_transform/input/sens/Regesten Sens A.docx")
@@ -32,16 +32,47 @@ if __name__ == '__main__':
     try:
         input_tei_path = convert_docx_to_tei(input_docx_path)
     except NotImplementedError:
-        logging.warning('please implement convert_docx_to_tei()')
+        logger.warning('please implement convert_docx_to_tei()')
         input_tei_path = make_proper_path("~/git/scripts/formel_transform/input/sens/Regesten Sens A.xml")
+    output_paths = []
     for input_tei_path in [make_proper_path("~/git/scripts/formel_transform/input/sens/Regesten Sens A.xml"), 
                            make_proper_path("~/git/scripts/formel_transform/input/sens/Regesten Sens B.xml")]:
-        subcorpus_name=''
         corpus_information_dict = get_corpus_information(input_tei_path)
         corpus_name = corpus_information_dict["corpus"]
-        if corpus_information_dict["subcorpus_index"]!='': subcorpus_name = "_a"
+        if corpus_information_dict["subcorpus_index"]=='': 
+            subcorpus_name = "_a"
+        else:
+            subcorpus_name = "_"+corpus_information_dict["subcorpus_index"]+"_"
 
-        output_path = make_proper_path("~/git/scripts/formel_transform/output/{corpus}/data/regesten/urn:cts:formulae:{corpus}{subcorpus}_regesten.xml".format(corpus=corpus_name, subcorpus=subcorpus_name))
+        output_path = make_proper_path("~/git/scripts/formel_transform/output/{corpus}/regesten/urn:cts:formulae:{corpus}{subcorpus}_regesten.xml".format(corpus=corpus_name, subcorpus=subcorpus_name))
         
-        subprocess_run(commands=['java', '-jar',  saxon_location, '-s:{}'.format(input_tei_path), '-o:{}'.format(output_path), transformation_file], logging=logging)
-        logging.info("Success! {} was converted to {} and then transformed to {} ".format(input_docx_path, input_tei_path, output_path))
+        
+        
+        subprocess_run(commands=['java', '-jar',  saxon_location, '-s:{}'.format(input_tei_path), '-o:{}'.format(output_path), transformation_file], logger=logger)
+        logger.info("Success! {} was converted to {} and then transformed to {} ".format(input_docx_path, input_tei_path, output_path))
+        output_paths.append(output_path)
+    
+    import xml.etree.ElementTree as ET
+    tree = ET.parse(output_paths[0])
+    root = tree.getroot()
+    for child in root:
+        if "regest" == child.tag:
+            if "docId" in child.attrib.keys():
+                child.set('docId', "urn:cts:formulae:{corpus}.form{subcorpus}{form_num}".format(corpus=corpus_name, 
+                                                                                                            subcorpus=subcorpus_name,
+                                                                                                            form_num=child.get('docId')))
+                print(child.get('docId'))
+    tree_two = ET.parse(output_paths[1])
+    root_two = tree_two.getroot()
+    for child in root_two:
+        if "regest" == child.tag:
+            if "docId" in child.attrib.keys():
+                child.set('docId', "urn:cts:formulae:{corpus}.form{subcorpus}{form_num}".format(corpus=corpus_name, 
+                                                                                                            subcorpus=subcorpus_name,
+                                                                                                            form_num=child.get('docId')))
+                print(child.get('docId'))
+    root.extend(root_two)
+    ET.indent(tree, space='  ', level=0)
+    print(ET.tostring(root, encoding='utf8'))
+    final_output_path = make_proper_path("~/git/scripts/formel_transform/output/{corpus}/regesten/urn:cts:formulae:{corpus}_regesten_master.xml".format(corpus=corpus_name))
+    tree.write(final_output_path)
