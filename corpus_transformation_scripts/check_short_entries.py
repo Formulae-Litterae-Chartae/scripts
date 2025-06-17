1645c2d339cd0a89b354ee9b530067ee9aa0d18b
tschomacker <thorben.schomacker@gmail.com>
2025-06-17 16:26:26 +0200

    #18 add error message for the problem

--- corpus_transformation_scripts/check_short_entries.py
+++ corpus_transformation_scripts/check_short_entries.py
@@ -45,22 +45,60 @@ saxon_location = args.saxon_location
 scripts_folder = args.scripts_folder
 add_bibl_xslt = scripts_folder+"/corpus_transformation_scripts/Formulae/add_missing_bibl_links.xsl"
 
-kurz = [re.sub('[„“"\'’]', '', x) for x in etree.parse(scripts_folder + '/bibliography/formulae_bibliographie.xml').xpath('//tei:title[@type="short"]/text()', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})]
+bibliography_xml_path = scripts_folder + '/bibliography/formulae_bibliographie.xml'
+
+kurztitel_list:list[str] = [re.sub('[„“"\'’]', '', x) for x in etree.parse(bibliography_xml_path).xpath('//tei:title[@type="short"]/text()', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'})]
+
+def check_kurztitel(kurztitel_list: list[str], bibliography_xml_path: str, logging) -> bool:
+    """
+    Checks whether any of the given 'Kurztitel' strings contain unwanted LaTeX encodings.
+
+    These LaTeX encodings may cause downstream processing errors if not removed.
+    The function scans for known problematic LaTeX control sequences and logs an error 
+    if any are found.
+
+    Parameters:
+        kurztitel_list (list[str]): A list of Kurztitel strings to check.
+        bibliography_xml_path (str): The path to the bibliography XML file (for logging context).
+        logging: A logger instance for error reporting.
+
+    Returns:
+        bool: True if no LaTeX encodings were found, False otherwise.
+    """
+    latex_encodings = ['\\glqq', '\\grqq', '\\S']  # properly escaped backslashes for raw strings
+    latex_encodings_found: set[str] = set()
+
+    for kurztitel in kurztitel_list:
+        for latex_encoding in latex_encodings:
+            if latex_encoding in kurztitel:
+                latex_encodings_found.add(latex_encoding)
+
+        if latex_encodings_found:
+            logging.error(
+                'LaTeX encodings found in {}: {}. '
+                'Remove them to avoid errors later.'.format(bibliography_xml_path, ', '.join(set(latex_encodings_found)))
+            )
+            return False
+
+    return True
+
+check_kurztitel(kurztitel_list, bibliography_xml_path, logging) 
 
 if 'data' in orig:
     glob_str = orig + '/**/*.xml'
 else:
     glob_str = orig + '/data/**/*.xml'
 
-texts = [x for x in glob(glob_str, recursive=True) if '__capitains__' not in x]
+texts:list[str] = [x for x in glob(glob_str, recursive=True) if '__capitains__' not in x]
 
 problems:list[tuple] = []
 
 for text in sorted(texts):
     for title in etree.parse(text).xpath('//tei:bibl', namespaces={'tei': 'http://www.tei-c.org/ns/1.0'}):
         whole_title = ''.join(title.xpath('.//text()'))
-        if re.sub('[„“"\'’]', '', whole_title.strip()) not in kurz:
-            closest = get_close_matches(whole_title, kurz, n=1, cutoff=0.8)          
+        whole_title_cleaned = re.sub('[„“"\'’]', '', whole_title.strip())
+        if whole_title_cleaned not in kurztitel_list:
+            closest = get_close_matches(whole_title, kurztitel_list, n=1, cutoff=0.8)          
             logging.debug("{} {} {}".format(text, re.sub('[„“"\'’]', '', whole_title.strip()), closest))                                                       
             problems.append((text.split('/')[-1], whole_title, closest[0] if closest else 'FEHLT'))
         elif title.get('n').strip() in ('', ','):
@@ -72,11 +110,12 @@ problem_path_location = Path(scripts_folder+'/results/elex_problems.txt')
 
 df = pd.DataFrame.from_records(problems)
 df.to_csv(problem_path_location, sep='\t')
-df.to_excel(str(problem_path_location).replace('txt', 'xlsx')) 
+df.columns = ['Datei', 'Referenz_in_Datei', 'Referenz_Citavi']
+df.to_excel(os.path.join(scripts_folder, 'results', 'fehlende_kurztitel.xlsx')) 
 
 if 0 == len(texts):
     logging.warning('No texts were found in {}. Please change the path'.format(glob_str))
 elif 0 == len(problems):
-    logging.info('No problems detected in {}. Identified {} texts {} kurz'.format(glob_str, len(texts), len(kurz)))
+    logging.info('No problems detected in {}. Identified {} texts {} kurz'.format(glob_str, len(texts), len(kurztitel_list)))
 else:
     logging.warning('{} problems detected in {}. Please the error log at {}'.format(len(problems),glob_str, problem_path_location))
